63e6846d6fdb451d723d6bb3e8edb6c33f22b106
logitha <loga848.456@gmail.com>
2016-07-05 09:28:52 +0530

    Rename a12.c to a122.c

---
+++ a122.c
@@ -0,0 +1,39 @@
+#include<bits/std++.h>
+using namespace std;
+struct Node
+{
+int key;
+struct Node* left, *right;
+};
+Node newNode(int key)
+{
+Node *temp=new Node;
+temp->key=key;
+temp->left=temp->right=NULL;
+return (temp);
+}
+bool isMirror(struct Node *root1, struct Node *root2)
+{
+if(root1==NULL && root2 ==NULL)
+return true;
+if(root1 && root2 && root1->key == root2->key)
+return isMirror(root1->left, root2->right)&&
+isMirror(root1->right, root2->left);
+return false;
+}
+bool idSymmetric(struct Node* root)
+{
+return isMirror(root, root);
+}
+int main()
+{
+Node *root = newNode(1);
+root->left =newNode(2);
+root->right = newNode(2);
+root->left->left=newNode(3);
+root->left->right=newNode(4);
+root->right->left=newNode(4);
+root->right->right=newNode(3);
+cout(is Symmetric(root);
+return 0;
+}
